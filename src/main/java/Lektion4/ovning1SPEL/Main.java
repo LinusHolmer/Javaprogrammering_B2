1c802c97bdc6e4a838ad380b10cd138b3f675abb
LinusHolmer <linus.holmer@stud.sti.se>
2024-10-21 11:50:11 +0200

    forts på spelet

--- src/main/java/Lektion4/ovning1SPEL/Main.java
+++ src/main/java/Lektion4/ovning1SPEL/Main.java
@@ -1,28 +1,8 @@
 package Lektion4.ovning1SPEL;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        boolean running = true;
-
-        while (running){
-            System.out.println("go north\ngo south\ngo east\ngo west\nquit");
-            String userInput = scan.nextLine();
-
-            switch(userInput){
-                case "go north" -> System.out.println("Going north");
-                case "go south" -> System.out.println("Going south");
-                case "go east" -> System.out.println("Going east");
-                case "go west" -> System.out.println("Going west");
-                case "quit" -> running = false;
-                default -> System.out.println("Invalid input");
-            }
-
-
-        }
-
+        Game newGame = new Game();
+        newGame.start();
     }
 }
