6751deb7492c38c28fb44d5c87a7bc670d6b1cad
LinusHolmer <linus.holmer@stud.sti.se>
2024-10-21 15:53:17 +0200

    hej

--- src/main/java/Lektion4/ovning1SPEL/Game.java
+++ src/main/java/Lektion4/ovning1SPEL/Game.java
@@ -5,86 +5,41 @@ import java.util.Scanner;
 public class Game {
     private boolean running;
     private Scanner scan = new Scanner(System.in);
-    private static final String WEST = "west";
-    private static final String EAST = "east";
-    private static final String NORTH = "north";
-    private static final String SOUTH = "south";
-    private static final String TOWN_CENTER = "town center";
-    private static final String START = "start";
-    private static String currentLocation = START;
+    private GameMap gameMap;
 
+    public Game(){
+        gameMap = new GameMap();
+    }
     public void start(){
         running = true;
         printWelcomeMenu();
-        townCentre();
+
         while (running){
             String userInput = getUserInput();
             running = processInput(userInput);
+            gameMap.getPosition();
         }
-
     }
-
     public void printWelcomeMenu(){
-        System.out.println("Welcome to Game, type direction to go in");
-        System.out.println("go north\ngo south\ngo east\ngo west\ngo town\nquit");
+        System.out.println("Welcome to Game! You can move in directions or quit.");
+        System.out.println("Commands: north, south, east, west, town, quit");
     }
     public String getUserInput(){
         return scan.nextLine();
     }
     public boolean processInput(String input) {
         switch (input) {
-            case "go north" -> north();
-            case "go south" -> south();
-            case "go east" -> east();
-            case "go west" -> west();
-            case "go town" -> townCentre();
+            case "north" -> gameMap.moveNorth();
+            case "south" -> gameMap.moveSouth();
+            case "east" -> gameMap.moveEast();
+            case "west" -> gameMap.moveWest();
+            case "town" -> gameMap.moveTown();
+
             case "quit" -> {
                 return false;
             }
             default -> System.out.println("Invalid input");
         }
         return true;
-    }
-
-    public void townCentre(){
-        if(!currentLocation.equals(TOWN_CENTER)){
-            System.out.println("You are in the middle of town, whats next?");
-            currentLocation = TOWN_CENTER;
-        } else{
-            System.out.println("You cant go there");
-        }
-
-    }
-    public void north(){
-        if(currentLocation.equals(TOWN_CENTER)){
-            currentLocation = NORTH;
-            System.out.println("You go north");
-        } else {
-            System.out.println("You cant go here");
-        }
-    }
-    public void south(){
-        if(currentLocation.equals(TOWN_CENTER)){
-            currentLocation = SOUTH;
-            System.out.println("You go south");
-        } else {
-            System.out.println("You cant go here");
-        }
-    }
-    public void east(){
-        if(currentLocation.equals(TOWN_CENTER)){
-            currentLocation = EAST;
-            System.out.println("You go east");
-        } else {
-            System.out.println("You cant go here");
-        }
-    }
-    public void west(){
-        if(currentLocation.equals(TOWN_CENTER)){
-            currentLocation = WEST;
-            System.out.println("You go west");
-        } else {
-            System.out.println("You cant go here");
-        }
     }
 }
---
+++ src/main/java/Lektion4/ovning1SPEL/GameMap.java
@@ -0,0 +1,39 @@
+package Lektion4.ovning1SPEL;
+
+public class GameMap {
+    private int x,y;
+
+    public GameMap(){
+        x = 0;
+        y = 0;
+    }
+
+    public void moveNorth(){
+        y++;
+        System.out.println("You moved north");
+    }
+    public void moveSouth(){
+        y--;
+        System.out.println("You move south");
+    }
+    public void moveEast(){
+        x++;
+        System.out.println("You move east");
+    }
+    public void moveWest(){
+        x--;
+        System.out.println("You move west");
+    }
+    public String getPosition(){
+        return "Player is at (" + x + " , " + y +")";
+    }
+    public void moveTown(){
+        if (x == 0 && y == 0) {
+            System.out.println("You are already there");
+        } else {
+            x = 0;
+            y = 0;
+            System.out.println("You move back to town");
+        }
+    }
+}
